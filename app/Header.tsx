80fcff371e2a6da50ed0b20313ebf837409990f3
Elan <54599408+ElanMedoff@users.noreply.github.com>
2023-04-17 23:40:41 -0400

    simplify header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,8 +1,4 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { twMerge as tm } from "tailwind-merge";
-import { transitionProperties } from "@/utils/styleHelpers";
 import { Chango } from "next/font/google";
 import avocado from "@/app/avocado.png";
 import Image from "next/image";
@@ -14,49 +10,8 @@ const font = Chango({
 });
 
 export default function Header() {
-  const [scrollDir, setScrollDir] = useState<"down" | "up">("up");
-
-  // https://stackoverflow.com/a/62497293
-  useEffect(() => {
-    const threshold = 20;
-    let lastScrollY = window.pageYOffset;
-    let ticking = false;
-
-    const updateScrollDir = () => {
-      const scrollY = window.pageYOffset;
-
-      if (Math.abs(scrollY - lastScrollY) < threshold) {
-        ticking = false;
-        return;
-      }
-      setScrollDir(scrollY > lastScrollY ? "down" : "up");
-      lastScrollY = scrollY > 0 ? scrollY : 0;
-      ticking = false;
-    };
-
-    const onScroll = () => {
-      if (!ticking) {
-        window.requestAnimationFrame(updateScrollDir);
-        ticking = true;
-      }
-    };
-
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [scrollDir]);
-
   return (
-    <nav
-      className={tm(
-        "fixed left-0 z-2 py-3 border-b-2 border-neutral bg-base-100 w-full",
-        scrollDir === "up" ? "top-0" : "top-[-100px]"
-      )}
-      style={{
-        ...transitionProperties,
-        transitionProperty: "top",
-      }}
-    >
+    <nav className="border-b-2 py-3 border-neutral bg-base-100 w-full">
       <div className="md:mx-10 flex items-center gap-3 justify-center md:justify-start">
         <Image src={avocado} alt="avocado ocarina" width={30} />
         <h1 className={tm("text-4xl text-neutral select-none", font.className)}>
--- app/Wrapper.tsx
+++ app/Wrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Wrapper({ children }: { children: ReactNode }) {
   return (
-    <main className="pt-[75px] min-h-[calc(100vh_-_62px)] px-4 md:px-10">
+    <main className="min-h-[calc(100vh_-_154px)] px-4 md:px-10">
       {children}
     </main>
   );
