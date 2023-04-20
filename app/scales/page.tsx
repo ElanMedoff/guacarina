b08fd7d49bdf88cc0f0c5da24769592df8423aef
Elan <54599408+ElanMedoff@users.noreply.github.com>
2023-04-19 20:27:35 -0400

    update default state

--- app/scales/page.tsx
+++ app/scales/page.tsx
@@ -66,7 +66,7 @@ export default function Home({ searchParams }: { searchParams: SearchParams }) {
     variantsParam ? Boolean(parseInt(variantsParam)) : false
   );
   const [showAllNotes, setShowAllNotes] = useState(
-    allParam ? Boolean(parseInt(allParam)) : false
+    allParam ? Boolean(parseInt(allParam)) : true
   );
   const scale =
     scalePattern === "major"
